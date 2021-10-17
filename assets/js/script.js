b45ecd685d91ca51e9e48a5c98495d6dcb78c8c0
ening <ening6649@yahoo.com>
2021-10-17 12:58:44 -0400

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,41 +21,67 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 // more on timer , video 4.1.7
 // buttonEl.addEventListener("click", function() {
 
-// by passing the event argument to the createTaskHandler() function , we can use the data and funcitonalilty that object holds
-var createTaskHandler = function(event) {   
+// by passing the event argument to the taskFormHandler() function , we can use the data and funcitonalilty that object holds
+var taskFormHandler = function(event) {   
     event.preventDefault();
     // single '' should be used inside a "" because otherwise the double quote would end early 
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    // console.dir(taskNameInput);
-    // creating a new list item. 
+
+    // package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    // send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+}
+//     // console.dir(taskNameInput);
+//     // creating a new list item. 
+//     var listItemEl = document.createElement("li");
+//     // dynamic styling . assign this class to the task item with the property className
+//     listItemEl.className = "task-item";
+//     // create div to hold task info and add to list item
+//     var taskInfoEl = document.createElement("div");
+//     // give it a class name
+//     taskInfoEl.className = "task-info";
+//     // add HTML content to div
+//     // textContent only accepts text values, here it would display html tags too
+//     // innerHTML reads the tags value and text values 
+//     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+//     // more explaination 4.2.6
+//     listItemEl.appendChild(taskInfoEl);
+//     // giving the list item text of this is a new task
+//     // listItemEl.textContent = taskNameInput;
+//     // to attach the task item as a child to the list
+//     tasksToDoEl.appendChild(listItemEl);
+// };
+
+var createTaskEl = function (taskDataObj) {
+    // create list item
     var listItemEl = document.createElement("li");
-    // dynamic styling . assign this class to the task item with the property className
     listItemEl.className = "task-item";
+
     // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
-    // give it a class name
     taskInfoEl.className = "task-info";
-    // add HTML content to div
-    // textContent only accepts text values, here it would display html tags too
-    // innerHTML reads the tags value and text values 
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-    // more explaination 4.2.6
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+
     listItemEl.appendChild(taskInfoEl);
-    // giving the list item text of this is a new task
-    // listItemEl.textContent = taskNameInput;
-    // to attach the task item as a child to the list
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
-};
+}
 
 // the createtakehandler should be before the below because we d be calling the function
 // before we defined it
-// the function below use createtaskhandler as the callback function
-// buttonEl.addEventListener("click", createTaskHandler);
+// the function below use TaskfromHandler as the callback function
+// buttonEl.addEventListener("click", taskFormHandler);
 // submit listens for when a user click a button element with a type attribut that has
 // a value of submit, and when a user presses Enter on the keyboard
 // Keeping a click event would trigger everytime the form is clicked
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
 
 // event.preventdefault() prevents the page from refreshing when a button is clicked. 
 // The common verb that's used for retrieving or reading data from an object's property is getting. 
